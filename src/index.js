138bf34500d8665b19c096546fb02edf420f81a3
unknown <yoswal99@gmail.com>
2023-08-20 12:58:58 +0530

    Created basic express framework, modularized, env variable port configured. Gitignore set up

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import express from "express";
+import dotenv from "dotenv";
+
+const app = express();
+dotenv.config();
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running at http://localhost:${port}`);
+});
