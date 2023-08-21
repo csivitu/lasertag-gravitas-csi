511e83c40cde8c0a743d53143448420c28774f36
unknown <yoswal99@gmail.com>
2023-08-21 14:55:51 +0530

    Created user and slot schemas and models. References between the models.

---
+++ src/models/slotModel.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+import User from "./userModel.js";
+
+const slotSchema = new mongoose.Schema({
+    startTime: {type: Date, required: true},
+    endTime: {type: Date, required: true},
+    day: {type: Number, default: 0},
+    slotBookedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
+});
+
+const Slot = mongoose.model('Slot', slotSchema);
+
+export default Slot;
