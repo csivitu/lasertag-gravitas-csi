25c45ec43dccb273b998e0575be3b816c99ffd26
unknown <yoswal99@gmail.com>
2023-08-22 09:23:25 +0530

    Started with registration controller, finished authentication middleware and helper functions

---
+++ src/controllers/registrationcontroller.js
@@ -0,0 +1,9 @@
+import catchAsync from "../helpers/catchAsync";
+
+const RegistrationController = catchAsync(
+    async (req, res, next) => {
+        let {username, email} = req.body;
+
+        
+    }
+);
