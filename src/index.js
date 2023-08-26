a91b70efbd7877320e813257859b1278f5b02797
Yug-Oswal <yoswal071@gmail.com>
2023-08-26 12:05:52 +0530

    Setup login and verify user routes to be used by app

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
+import login from "./routes/login";
+import verifyuser from "./routes/verifyuser";
 
 const app = express();
 dotenv.config();
@@ -8,3 +10,6 @@ const port = process.env.PORT || 3000;
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}`);
 });
+
+app.use("/login", login);
+app.use("/verify-user", verifyuser);
