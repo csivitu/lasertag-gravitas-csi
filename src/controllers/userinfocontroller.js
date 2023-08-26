3fc5373decc27c7f216d11b98208c9ff9ac6e561
Yug-Oswal <yoswal071@gmail.com>
2023-08-26 12:27:20 +0530

    Created UserInfo controller; Modified slotModel to remove Venue field

---
+++ src/controllers/userinfocontroller.js
@@ -0,0 +1,18 @@
+import catchAsync from "../helpers/catchAsync";
+import User from "../models/userModel";
+
+const UserInfoController = catchAsync(
+    async (req, res) => {
+        const userID = req.userID;
+
+        const user = await User.findOne({_id: userID}).populate("slotBooked")
+        .catch((err) => {
+            console.log("Error retrieving User Info (from DB): " + err.message);
+            return res.status(500).json({error: "Error retrieving User Info"});
+        });
+
+        return res.status(200).json({regno: user.regno, email: user.email, slotBooked: user.slotBooked});
+    }
+);
+
+export default UserInfoController;
--- src/models/slotModel.js
+++ src/models/slotModel.js
@@ -6,7 +6,6 @@ const slotSchema = new mongoose.Schema({
     endTime: {type: Date, required: true},
     day: {type: Number, default: 0},
     slotBookedBy: {type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true},
-    venue: {type: String, required: true}
 });
 
 const Slot = mongoose.model('Slot', slotSchema);
