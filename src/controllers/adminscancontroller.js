afb61e8a215981552f4995eaa6b950fe600d7d63
salt57 <sourishgupta02@gmail.com>
2023-09-17 13:46:43 +0530

    chore: return slot details when scanned

--- src/controllers/adminscancontroller.js
+++ src/controllers/adminscancontroller.js
@@ -4,7 +4,7 @@ import User from "../models/userModel.js";
 
 const AdminScan = catchAsync(async (req, res) => {
   const { regno } = req.params;
-  const user = await User.find({ regno: regno });
+  const user = await User.find({ regno: regno }).populate("slotBooked");
   if (!user) {
     return res.status(400).json({ error: "Invalid User ID" });
   }
@@ -15,7 +15,7 @@ const AdminScan = catchAsync(async (req, res) => {
   await user.save();
 
   Logger.info(`Successfully scanned QR code for user ${regno}.`);
-  return res.status(200).json({ scanned: true });
+  return res.status(200).json({ scanned: true, slot: user.slotBooked });
 });
 
 export default AdminScan;
