120c4be3484ce7d68700506cf7a11e6e316d7665
Yug-Oswal <yoswal071@gmail.com>
2023-09-18 23:50:45 +0530

    Created: super admin middleware

---
+++ src/middleware/requireSupAdmin.js
@@ -0,0 +1,19 @@
+import User from "../models/userModel.js";
+import catchAsync from "../helpers/catchAsync.js";
+
+const requireAdmin = catchAsync(
+    async (req, res, next) => {
+        const {userID} = req.userID;
+
+        const user = await User.findById(userID);
+
+        if (user.scope != "SUPERADMIN") {
+            return res.status(400).json({error: "User not authorized as SUPER ADMIN"});
+        }
+
+        req.admin = {adminMail: user.email};
+        next();
+    }
+);
+
+export default requireAdmin;
