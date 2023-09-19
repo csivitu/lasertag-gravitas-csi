ab99cd11e1c6a407a156c6dad053cc527fd3fa2c
Yug-Oswal <yoswal071@gmail.com>
2023-09-20 00:50:31 +0530

    updated: global rate limiter

--- src/middleware/apilimiter.js
+++ src/middleware/apilimiter.js
@@ -1,6 +1,6 @@
 import createLimiter from "../helpers/createlimiter.js";
 
 const msg = "Too many requests. Request limit exceeded. Please do not deliberately send requests for no reason.";
-const apilimiter = createLimiter(100, 15, msg);
+const apilimiter = createLimiter(15, 2, msg);
 
 export default apilimiter;
