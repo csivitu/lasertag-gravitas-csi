45e457cbead6a8925fbbb0f67e393838e07db4c6
Yug-Oswal <yoswal071@gmail.com>
2023-09-20 17:27:31 +0530

    updated: cancel slot route made admin

--- src/routes/cancelslot.js
+++ src/routes/cancelslot.js
@@ -2,9 +2,10 @@ import express from "express";
 import protect from "../middleware/protect.js";
 import CancelSlotController from "../controllers/cancelslotcontroller.js";
 import requireNotScanned from "../middleware/requireNotScanned.js";
+import requireAdmin from "../middleware/requireAdmin.js";
 
 const cancelslot = express.Router();
 
-cancelslot.post("/", protect,requireNotScanned, CancelSlotController);
+cancelslot.post("/", protect, requireAdmin, requireNotScanned, CancelSlotController);
 
 export default cancelslot;
