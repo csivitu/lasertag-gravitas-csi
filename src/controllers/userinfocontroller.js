6d8e02e19926fe03d03a1e96cdf8954dbc0deea3
Yug-Oswal <yoswal071@gmail.com>
2023-09-20 13:09:53 +0530

    update: user slot time adjustment

--- src/controllers/userinfocontroller.js
+++ src/controllers/userinfocontroller.js
@@ -1,6 +1,7 @@
 import catchAsync from "../helpers/catchAsync.js";
 import Logger from "../initializers/logger.js";
 import User from "../models/userModel.js";
+import moment from "moment-timezone";
 
 const UserInfoController = catchAsync(
     async (req, res) => {
@@ -13,7 +14,24 @@ const UserInfoController = catchAsync(
         });
 
         Logger.info(`${user.email} retrieved user info.`);
-        return res.status(200).json(user);
+
+        let adjustedUser;
+        if (user.slotBooked != null) {
+            const adjustedStartTime = moment.tz(user.slotBooked.startTime.getTime() - 10 * 60 * 1000, 'UTC');
+            const adjustedEndTime = moment.tz(user.slotBooked.endTime.getTime() - 10 * 60 * 1000, 'UTC');
+            adjustedUser = {
+                ...user.toObject(),
+                slotBooked: {
+                    ...user.slotBooked.toObject(),
+                    startTime: adjustedStartTime,
+                    endTime: adjustedEndTime
+                }
+            };
+        } else {
+            adjustedUser = user;
+        }
+
+        return res.status(200).json(adjustedUser);
     }
 );
 
