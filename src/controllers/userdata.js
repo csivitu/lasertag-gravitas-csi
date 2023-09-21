51fcdf8b2ab5ddfe4f2b370a770444b5e94cf11d
Yug-Oswal <yoswal071@gmail.com>
2023-09-22 01:50:18 +0530

    added: userdata

---
+++ src/controllers/userdata.js
@@ -0,0 +1,252 @@
+const userdata = [
+    {
+      "startTime": "2023-09-22T03:45:00.000Z",
+      "users": [
+        {
+          "phone": "9302350315",
+          "email": "rakshit.ashtputre2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T04:00:00.000Z",
+      "users": [
+        {
+          "phone": "7013597716",
+          "email": "aasasingh.bhui2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8977514842",
+          "email": "arav.singh2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9154539701",
+          "email": "tanmay.khambete2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8766744138",
+          "email": "aayushhemant.singh2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T04:15:00.000Z",
+      "users": [
+        {
+          "phone": "9625511813",
+          "email": "keshav.arora2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "6289715383",
+          "email": "ankur.chatterjee2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T04:30:00.000Z",
+      "users": [
+        {
+          "phone": "9946499555",
+          "email": "niharika.lathish2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9151110141",
+          "email": "pranshu.agarwal2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7704049099",
+          "email": "devaryan.jain2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7337664560",
+          "email": "sherin.anaya2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8122603493",
+          "email": "lisa.ghimire2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9142273150",
+          "email": "akshat.srivastwa2022@vitstudent.ac.in"
+        },
+        {
+          "phone": "9650606105",
+          "email": "ruchit.gupta2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7249883668",
+          "email": "pranjal.basagare2022@vitstudent.ac.in"
+        },
+        {
+          "phone": "7983532930",
+          "email": "madhav.juneja2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T04:45:00.000Z",
+      "users": [
+        {
+          "phone": "7737929601",
+          "email": "aashman.gupta2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "6371217412",
+          "email": "ayush.mahapatra2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8104158875",
+          "email": "mrinali.prafull2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9686320350",
+          "email": "mayank.mohanty2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9539495616",
+          "email": "shravansunoj.nair2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T05:00:00.000Z",
+      "users": [
+        {
+          "phone": "7976453836",
+          "email": "jai.parwani2020@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T05:15:00.000Z",
+      "users": [
+        {
+          "phone": "8240932196",
+          "email": "saishree.behera2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "6202239772",
+          "email": "aashish.kumar2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8669158975",
+          "email": "shlok.mithilesh2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7669659429",
+          "email": "aditya.garg2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7905657678",
+          "email": "yogendra.singh2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9997045194",
+          "email": "tanay.gupta2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T05:30:00.000Z",
+      "users": [
+        {
+          "phone": "8920461139",
+          "email": "moksh.punn2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9747422303",
+          "email": "sarang.brijesh2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9666409025",
+          "email": "harshita.bogineni2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9391569649",
+          "email": "aryan.2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8977608499",
+          "email": "linga.nikhilarya@gmail.com"
+        },
+        {
+          "phone": "9789132819",
+          "email": "jami.shashank2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T05:45:00.000Z",
+      "users": [
+        {
+          "phone": "6364533214",
+          "email": "manan.amahajan2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T06:00:00.000Z",
+      "users": [
+        {
+          "phone": "8668232675",
+          "email": "guptarishabh.manish2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9877442754",
+          "email": "ayushman.mittal2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8127814585",
+          "email": "rishit.malik2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9834748536",
+          "email": "ciya.darshan2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8484996634",
+          "email": "siddhi.jitendra2023@vitstudent.ac.in"
+        }
+      ]
+    },
+    {
+      "startTime": "2023-09-22T06:15:00.000Z",
+      "users": [
+        {
+          "phone": "9321042787",
+          "email": "aniket.amitabh2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9037319639",
+          "email": "athulya.ajimon2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8848982052",
+          "email": "ananya.sathyababu2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7736752094",
+          "email": "rishikesh.sa2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "8425023833",
+          "email": "gargi.mahajan2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "7306103448",
+          "email": "nakul.m2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "6282565406",
+          "email": "bhagya.lakshmi2023@vitstudent.ac.in"
+        },
+        {
+          "phone": "9591474545",
+          "email": "mdaamirzahoor@gmail.com"
+        }
+      ]
+    }
+]
+
+export default userdata;
+
