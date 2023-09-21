fa76172a16b64b6e60061cb4534285222625fa5e
Yug-Oswal <yoswal071@gmail.com>
2023-09-21 10:36:35 +0530

    changed mailer

--- src/controllers/sendmailcontroller.js
+++ src/controllers/sendmailcontroller.js
@@ -2,6 +2,7 @@ import User from "../models/userModel.js";
 import catchAsync from "../helpers/catchAsync.js";
 import Logger from "../initializers/logger.js";
 import envHandler from "../helpers/envHandler.js";
+import ses from "../initializers/sesmailer.js";
 import fs from "fs";
 
 const SendMailController = catchAsync(
@@ -11,36 +12,67 @@ const SendMailController = catchAsync(
             Logger.info('Wrong password entered for creating data');
             return res.status(400).json({error: "Bad auth: You are not allowed to create data."});
         }
-        
-        const users = await User.find({});
 
         const html = fs.readFileSync('/app/src/controllers/noqr.html', 'utf8');
 
-        for (let user of users) {
+        const users = await User.find({scope: "SUPERADMIN"}, 'email').lean().exec(); // To be changed to all users
+        const emailList = users.map((user) => user.email);
+        console.log(emailList);
+        console.log(emailList.length);
 
-            fetch(envHandler('MAILER'), {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/json'
+        const batchSize = 14;
+        const emailsPerSecond = 14;
+        const delayBetweenBatches = 1000;
+
+        async function sendEmailBatch(emailBatch) {
+            const promises = emailBatch.map((email) => {
+              const params = {
+                Source: 'Team CSI <askcsivit@gmail.com>', // Replace with your sender email
+                Destination: {
+                  ToAddresses: [email],
                 },
-                body: JSON.stringify({
-                    to: user.email,
-                    from: "Team CSI <Askcsivit@gmail.com>",
-                    subject: "Slot Booking Notice for LaserTag",
-                    html: html,
-                    auth: envHandler('MLRPASS')
+                Message: {
+                  Subject: {
+                    Data: 'Slot Booking Notice for LaserTag - CSI',
+                  },
+                  Body: {
+                    Html: {
+                      Data: html,
+                    },
+                  },
+                },
+              };
+          
+              return ses.sendEmail(params).promise()
+                .then(() => {
+                  console.log(`Email sent to: ${email}`);
                 })
-            })
-            .then((info) => {
-                Logger.info(`${user.email} got the notice mail successfully: ${info}`);
-            })
-            .catch((err) => {
-                Logger.error(`Mailer Error: ${err}: Unable to send mail for ${user.email} for notice mail.`);
+                .catch((err) => {
+                  console.error(`Error sending email to ${email}: ${err.message}`);
+                });
             });
+
+            await Promise.all(promises);
+        };
+
+        async function sendBulkEmails() {
+            while (emailList.length > 0) {
+              const emailBatch = emailList.splice(0, batchSize);
+              await sendEmailBatch(emailBatch);
+          
+              if (emailList.length > 0) {
+                // Introduce a delay between batches to stay within the SES rate limit
+                await new Promise((resolve) => setTimeout(resolve, delayBetweenBatches));
+              }
+            }
+          
+            console.log('All emails sent successfully');
         }
 
+        sendBulkEmails()
+        .catch((err) => console.error(`Error sending bulk emails: ${err}`));
+
         return res.status(200).json({message: "Sent mail successfully."});
-    }
-);
+});
 
 export default SendMailController;
