abef9ed34330969d24f87ce88e16ae195d379848
Yug-Oswal <yoswal071@gmail.com>
2024-02-28 22:47:54 +0530

    update: new riviera data; mail template

--- src/controllers/createuserdatacontroller.js
+++ src/controllers/createuserdatacontroller.js
@@ -17,6 +17,86 @@ const CreateUserDataController = catchAsync(
             return res.status(400).json({error: "Bad auth: You are not allowed to create data."});
         }
 
+        const filepath = "/app/userdata.xlsx";
+        const workbook = XLSX.readFile(filepath);
+
+        const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+
+        const jsonUsers = XLSX.utils.sheet_to_json(worksheet);
+        
+        for (let user of jsonUsers) {
+            let userName, userRegNo, userEmail, userPhone;
+            userName = (user['Student Name']).trim();
+            if (user['Register Number']) { 
+                userRegNo = (user['Register Number']).trim();
+            } else {userRegNo = null;}
+            userEmail = (user['EmailId']).trim().toLowerCase();
+            userPhone = ((user['MobileNo.']).toString()).trim();
+            
+            let newUser = {
+                name: userName,
+                regno: userRegNo,
+                email: userEmail,
+                phoneno: userPhone
+            };
+
+            await User.create([newUser])
+            .catch((err) => {
+                Logger.error(`Error creating user: ${err}`);
+                return res.status(500).json({error: `Error occurred when creating new user: ${err}`});
+            })
+        }
+
+        // await User.create([
+        //     {
+        //         name: "Anubhav Aryan",
+        //         email: "anubhav.aryan2021@vitstudent.ac.in",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "Sourish Gupta",
+        //         email: "sourishgupta02@gmail.com",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "Yug Oswal",
+        //         email: "yoswal071@gmail.com",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "Manas Laud",
+        //         email: "manasgaming126@gmail.com",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "I Love Goth",
+        //         email: "kaushalrathi24@gmail.com",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "Alisha Bandyopadhyay",
+        //         email: "alisha.bandyopadhyay2021@vitstudent.ac.in",
+        //         scope: "SUPERADMIN"
+        //     },
+        //     {
+        //         name: "Ansu Banerjee",
+        //         email: "ansu.banerjee2022@vitstudent.ac.in",
+        //         scope: "ADMIN"
+        //     },
+        //     {
+        //         name: "Ojas Tapadia",
+        //         email: "ojas.tapadia2022@vitstudent.ac.in",
+        //         scope: "ADMIN"
+        //     }
+        // ]);
+
+        // await User.findOneAndUpdate(
+        //     {email: "brinda.dhingra2020@vitstudent.ac.in"},
+        //     {$set: {scope: "SUPERADMIN"}}
+        // );
+
+        return res.status(400).json({message: "User Data successfully created."});
+
     //     const slots = await Slot.find({day: 3, isCarry: false})
     //     .populate("slotBookedBy");
 
