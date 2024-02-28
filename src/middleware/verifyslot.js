c47bbbe5a38139197be66040d918f5eca1b38c2f
Yug-Oswal <yoswal071@gmail.com>
2024-02-29 02:17:34 +0530

    update: minor fix

--- src/middleware/verifyslot.js
+++ src/middleware/verifyslot.js
@@ -4,7 +4,7 @@ import Slot from "../models/slotModel.js";
 
 const verifyslot = catchAsync(
     async (req, res, next) => {
-        return res.status(400).json({error: "Slot booking has now been officialy closed. Thank you for your participation!"});
+        // return res.status(400).json({error: "Slot booking has now been officialy closed. Thank you for your participation!"});
         let {slotId} = req.body;
         const slot = await Slot.findById(slotId).populate("slotBookedBy");
 
