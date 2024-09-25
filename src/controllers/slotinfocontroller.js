255ed06f73b380e80a77fe34c074eda58187f5a9
0ju1c3 <otapadia@gmail.com>
2024-09-26 01:46:49 +0530

    time test 2

--- src/controllers/slotinfocontroller.js
+++ src/controllers/slotinfocontroller.js
@@ -6,29 +6,29 @@ import moment from "moment-timezone";
 const SlotInfoController = catchAsync(
     async (req, res) => {
         const slots = await Slot.find({ toShow: true, isCarry: false })
-            .populate({ path: "slotBookedBy", select: "name" })
+            // .populate({ path: "slotBookedBy", select: "name" })
             .sort({ day: 1, startTime: 1 })
             .catch((err) => {
                 Logger.error(`Slot retrieval/sorting error (Get Slot Info Error):  + ${err.message}`);
                 return res.status(500).json({ error: "Unable to retrieve/sort Slots from Database" });
             });
 
-        // const adjustedSlots = slots.map((slot) => {
-        //     const adjustedStartTime = new moment.tz(slot.startTime.getTime(), 'UTC');
-        //     const adjustedEndTime = new moment.tz(slot.endTime.getTime(), 'UTC');
-        //
-        //     // Create a new object with adjusted times and other properties
-        //     return {
-        //         ...slot.toObject(),
-        //         startTime: adjustedStartTime,
-        //         endTime: adjustedEndTime,
-        //     };
-        // });
-        //
+        const adjustedSlots = slots.map((slot) => {
+            const adjustedStartTime = new moment.tz(slot.startTime.getTime(), 'UTC');
+            const adjustedEndTime = new moment.tz(slot.endTime.getTime(), 'UTC');
+
+            // Create a new object with adjusted times and other properties
+            return {
+                ...slot.toObject(),
+                startTime: adjustedStartTime,
+                endTime: adjustedEndTime,
+            };
+        });
+
         Logger.info("Successfully returned slot info as response.");
         Logger.info(`Returned slot info for: ${adjustedSlots.length} slots.`);
-        // return res.status(200).json(adjustedSlots);
-        return res.status(200).json(slots);
+        return res.status(200).json(adjustedSlots);
+        // return res.status(200).json(slots);
     }
 );
 
