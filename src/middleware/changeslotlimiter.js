0fe2e17d87794f045a2acafa6ed5d87c7d522736
0ju1c3 <otapadia@gmail.com>
2024-09-26 03:01:02 +0530

    change slot limiter

--- src/middleware/changeslotlimiter.js
+++ src/middleware/changeslotlimiter.js
@@ -1,6 +1,6 @@
 import createLimiter from "../helpers/createlimiter.js";
 
 const msg = "Cannot change slot more than twice in a day. Please try again later.";
-const changeslotlimiter = createLimiter(2, 24 * 60, msg);
+const changeslotlimiter = createLimiter(5, 24 * 60, msg);
 
 export default changeslotlimiter;
