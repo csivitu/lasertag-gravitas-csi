bc0937fe36aeb4cff0093302263196274bba0642
0ju1c3 <otapadia@gmail.com>
2024-09-26 02:07:52 +0530

    final3

--- src/controllers/slotinfocontroller.js
+++ src/controllers/slotinfocontroller.js
@@ -5,15 +5,30 @@ import moment from "moment-timezone";
 
 const SlotInfoController = catchAsync(
     async (req, res) => {
-        const slots = await Slot.find({ toShow: true, isCarry: false })
-            // .populate({ path: "slotBookedBy", select: "name" })
-            .sort({ day: 1, startTime: 1 })
-            .catch((err) => {
-                Logger.error(`Slot retrieval/sorting error (Get Slot Info Error):  + ${err.message}`);
-                return res.status(500).json({ error: "Unable to retrieve/sort Slots from Database" });
-            });
+        try {
+            const slots = await Slot.find({ toShow: true, isCarry: false })
+                .sort({ day: 1, startTime: 1 });
 
-        return res.status(200).json(slots);
+            const slotsInIST = slots.map(slot => ({
+                ...slot._doc,
+                startTime: moment(slot.startTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss'),
+                endTime: moment(slot.endTime).tz('Asia/Kolkata').format('YYYY-MM-DD HH:mm:ss'),
+            }));
+
+            return res.status(200).json(slotsInIST);
+        } catch (err) {
+            console.error(`Error retrieving slots: ${err.message}`);
+            return res.status(500).json({ error: "Unable to retrieve Slots from Database" });
+        }
+        // const slots = await Slot.find({ toShow: true, isCarry: false })
+        //     // .populate({ path: "slotBookedBy", select: "name" })
+        //     .sort({ day: 1, startTime: 1 })
+        //     .catch((err) => {
+        //         Logger.error(`Slot retrieval/sorting error (Get Slot Info Error):  + ${err.message}`);
+        //         return res.status(500).json({ error: "Unable to retrieve/sort Slots from Database" });
+        //     });
+        //
+        // return res.status(200).json(slots);
         // const adjustedSlots = slots.map((slot) => {
         //     const adjustedStartTime = new moment.tz(slot.startTime.getTime(), 'UTC');
         //     const adjustedEndTime = new moment.tz(slot.endTime.getTime(), 'UTC');
