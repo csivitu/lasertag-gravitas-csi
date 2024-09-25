8239542478e85fb787254eda18aad632ab915b83
0ju1c3 <otapadia@gmail.com>
2024-09-25 22:28:39 +0530

    changes to slotinfocontroller

--- src/controllers/slotinfocontroller.js
+++ src/controllers/slotinfocontroller.js
@@ -5,23 +5,23 @@ import moment from "moment-timezone";
 
 const SlotInfoController = catchAsync(
     async (req, res) => {
-        const slots = await Slot.find({toShow: true, isCarry: false, day: 1})
-        .populate({path: "slotBookedBy", select: "name"})
-        .sort({day: 1, startTime: 1})
-        .catch((err) => {
-            Logger.error(`Slot retrieval/sorting error (Get Slot Info Error):  + ${err.message}`);
-            return res.status(500).json({error: "Unable to retrieve/sort Slots from Database"});
-        });
+        const slots = await Slot.find({ toShow: true, isCarry: false })
+            .populate({ path: "slotBookedBy", select: "name" })
+            .sort({ day: 1, startTime: 1 })
+            .catch((err) => {
+                Logger.error(`Slot retrieval/sorting error (Get Slot Info Error):  + ${err.message}`);
+                return res.status(500).json({ error: "Unable to retrieve/sort Slots from Database" });
+            });
 
         const adjustedSlots = slots.map((slot) => {
             const adjustedStartTime = new moment.tz(slot.startTime.getTime() - 10 * 60 * 1000, 'UTC');
             const adjustedEndTime = new moment.tz(slot.endTime.getTime() - 10 * 60 * 1000, 'UTC');
-        
+
             // Create a new object with adjusted times and other properties
             return {
-              ...slot.toObject(),
-              startTime: adjustedStartTime,
-              endTime: adjustedEndTime,
+                ...slot.toObject(),
+                startTime: adjustedStartTime,
+                endTime: adjustedEndTime,
             };
         });
 
