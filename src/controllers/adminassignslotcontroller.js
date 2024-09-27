2d65cfeb7338de70b9a35b2e86e23931df19871c
0ju1c3 <otapadia@gmail.com>
2024-09-27 11:31:17 +0530

    feat: admin panel changes

--- src/controllers/adminassignslotcontroller.js
+++ src/controllers/adminassignslotcontroller.js
@@ -6,23 +6,23 @@ import Logger from "../initializers/logger.js";
 
 const AdminAssignSlotController = catchAsync(
     async (req, res) => {
-        let {adminMail} = req.admin;
-        let {email, slotId} = req.body;
+        let { adminMail } = req.admin;
+        let { email, slotId } = req.body;
         email = email.trim();
         email = email.toLowerCase();
 
-        const user = await User.findOne({email: email});
+        const user = await User.findOne({ email: email });
 
         if (!user) {
             Logger.info(`${email} is an invalid user email id for admin to cancel slot.`);
-            return res.status(400).json({error: "Invalid Email-ID entered."});
+            return res.status(400).json({ error: "Invalid Email-ID entered." });
         }
 
         const slot = await Slot.findById(slotId);
 
         if (!slot) {
             Logger.info(`ADMIN ${adminMail} attempted to assign invalid slot.`);
-            return res.status(400).json({error: "Invalid Slot / Slot not Found"});
+            return res.status(400).json({ error: "Invalid Slot / Slot not Found" });
         }
 
         if (user.slotBooked != null) {
@@ -40,7 +40,7 @@ const AdminAssignSlotController = catchAsync(
         // optional: send email with new qr code
         await Promise.all([user.save(), slot.save()]);
         Logger.info(`ADMIN ${adminMail} assigned slot ${slot.startTime} to ${email}.`);
-        return res.status(400).json({message: `Successfully assigned slot ${slot.startTime} to ${email}.`});
+        return res.status(200).json({ message: `Successfully assigned slot ${slot.startTime} to ${email}.` });
     }
 );
 
